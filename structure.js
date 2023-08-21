989417df904426329bc138a8fb41cab770edcf90
usumai <usumai@gmail.com>
2023-08-21 18:05:50 +1000

    Create auto gen site

---
+++ structure.js
@@ -0,0 +1,30 @@
+const jsonData = {
+    "datastructure.html": {
+        "doc_title": "Data view page",
+        "doc_order": 1,
+        "doc_id": 3,
+        "path": "datastructure.html"
+    },
+    "dataviews.html": {
+        "doc_title": "Data structure page",
+        "doc_order": 3,
+        "doc_id": 1,
+        "path": "dataviews.html"
+    },
+    "layouts": {
+        "layout.html": {
+            "doc_title": "Layout page",
+            "doc_order": 1,
+            "doc_id": 2,
+            "path": "layouts/layout.html"
+        },
+        "ai": {
+            "openai.html": {
+                "doc_title": "AI layout page",
+                "doc_order": 1,
+                "doc_id": 4,
+                "path": "layouts/ai/openai.html"
+            }
+        }
+    }
+}
