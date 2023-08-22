bdef35b41e48f8c131daa2e84830e2ecc9be15f0
usumai <usumai@gmail.com>
2023-08-22 12:03:35 +1000

    reengineered to work only with json

--- structure.js
+++ structure.js
@@ -1,32 +1,50 @@
 const jsonData = {
-    "DataSources": {
-        "datastructure.html": {
-            "doc_title": "Data structure page",
-            "doc_order": 1,
-            "doc_id": 3,
-            "path": "DataSources/datastructure.html"
-        }
+    "pages": {
+      "1": {
+        "id": 1,
+        "order_no": 6,
+        "title": "Home page",
+        "content": "<p>Hello World!</p><p><strong>hellow</strong></p><p><br></p><h1><strong>asdasdasdasd</strong></h1>",
+        "category": ""
+      },
+      "2": {
+        "id": 2,
+        "order_no": 3,
+        "title": "Data view page",
+        "content": "",
+        "category": "Data views"
+      },
+      "3": {
+        "id": 3,
+        "order_no": 2,
+        "title": "Layout page",
+        "content": "Layouts/layout.html",
+        "category": "Layouts"
+      },
+      "4": {
+        "id": 4,
+        "order_no": 1,
+        "title": "AI layout page",
+        "content": "Layouts/openai.html",
+        "category": "Layouts"
+      },
+      "5": {
+        "id": 5,
+        "order_no": 6,
+        "title": "Data structure page",
+        "content": "DataSources/datastructure.html",
+        "category": "Data sources"
+      }
     },
-    "DataViews": {
-        "dataviews.html": {
-            "doc_title": "Data view page",
-            "doc_order": 3,
-            "doc_id": 1,
-            "path": "DataViews/dataviews.html"
-        }
-    },
-    "Layouts": {
-        "layout.html": {
-            "doc_title": "Layout page",
-            "doc_order": 1,
-            "doc_id": 2,
-            "path": "Layouts/layout.html"
-        },
-        "openai.html": {
-            "doc_title": "AI layout page",
-            "doc_order": 1,
-            "doc_id": 4,
-            "path": "Layouts/openai.html"
-        }
+    "categories": {
+      "Layouts": {
+        "order_no": 4
+      },
+      "Data views": {
+        "order_no": 2
+      },
+      "Data sources": {
+        "order_no": 3
+      }
     }
-}
+  }
